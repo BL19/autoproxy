d35311a556ff96282195d169e670a98cea6d3373
BL19 <tecmans2344@gmail.com>
2019-12-20 20:41:57 +0100

    Config Update

--- src/main/java/me/BL19/AutoProxy/AutoProxyConfig.java
+++ src/main/java/me/BL19/AutoProxy/AutoProxyConfig.java
@@ -1,10 +1,12 @@
 package me.BL19.AutoProxy;
 
 import java.util.HashMap;
+import java.util.List;
 
 public class AutoProxyConfig {
 
 	public int port = 8080;
 	public HashMap<String, ProxyAddress> adresses;
+	public List<String> fileTypesToIgnore;
 	
 }
--- src/main/java/me/BL19/AutoProxy/ProxyAddress.java
+++ src/main/java/me/BL19/AutoProxy/ProxyAddress.java
@@ -5,5 +5,7 @@ public class ProxyAddress {
 	public String url;
 	public String suburl;
 	public boolean enabled = true;
+	public boolean hardReplace = false;
+	public boolean replaceInHeaders = false;
 	
 }
